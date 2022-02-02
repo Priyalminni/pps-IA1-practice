5b9287a0d88a71e3fa8bbe8db1f6abe16d6b503b
Priyalminni <93091977+Priyalminni@users.noreply.github.com>
2022-02-02 09:09:26 +0000

    execute

--- p1original.c
+++ p1original.c
@@ -1,2 +1,23 @@
 
+#include<stdio.h>
+void input(int *pa,int *pb)
+{
+  printf("entre a number\n");
+  scanf("%d%d",pa,pb);
+}
+void add(int a,int b,int*sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d + %d is %d\n",a,b,sum);
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
 }
